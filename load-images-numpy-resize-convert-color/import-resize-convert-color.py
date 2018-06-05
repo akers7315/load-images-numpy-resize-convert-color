1cfd8d20fc08242a935e08ef178b53b477668ce4
akers7315 <38360178+akers7315@users.noreply.github.com>
2018-06-05 11:47:14 -0500

    Update import-resize-convert-color.py

--- load-images-numpy-resize-convert-color/import-resize-convert-color.py
+++ load-images-numpy-resize-convert-color/import-resize-convert-color.py
@@ -26,16 +26,9 @@ This is a temporary script file.
 """
 
 #!/usr/bin/python
-import glob
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-from PIL import Image
-
 
 import glob
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 X = []
@@ -47,9 +40,6 @@ for filename in glob.glob('file-path\\*.jpg'): #assuming gif
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     resized_img = cv2.resize(gray_img, (200, 200))
     X.append(resized_img)
-    #plt.imshow(resized_img)
-    #plt.xticks([]), plt.yticks([]) 
-    #plt.show()
 
 X = np.asarray(X)
 print(X.shape)
